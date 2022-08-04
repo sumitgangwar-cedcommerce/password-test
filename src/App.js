0811aef49b1a5ab59cb8105b1a2cd75f623d803a
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-08-04 12:37:47 +0530

    upd

--- src/App.js
+++ src/App.js
@@ -6,26 +6,17 @@ function App() {
 
   const [password , setPassword] = useState('')
 
-  const letters = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' ,'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' ,'y' , 'z']
+  var letters = 'abcdefghijklmnopqrstuvwxyz1234567890@ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  
 
   const btnhandlers = (e)=>{
-    let first = (Math.random(1,100)*1000).toFixed(0)
+    let temp = letters.split('')
     let str = ''
-    str+=((Math.random(1,100)*1000).toFixed(0)).toString()
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? '8512@3' :letters[(Math.random(0-25)*10).toFixed(0)];
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Abq' : (letters[(Math.random(0-25)*10).toFixed(0)]).toUpperCase();
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Aasd' : (letters[(Math.random(0-25)*10).toFixed(0)]).toLowerCase();
-    str+=((Math.random(1,2)*1000).toFixed(0));
-    str+=((Math.random()*10).toFixed(0))>5 ? '@11a@' : '11@1'
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Aa' : (letters[(Math.random(0-25)*10).toFixed(0)]).toUpperCase();
-    str+=((Math.random(1,100)*1000).toFixed(0)).toString();
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Aqa' : (letters[(Math.random(0-25)*10).toFixed(0)]).toLowerCase();
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Ahjjj' : (letters[(Math.random(0-25)*10).toFixed(0)]).toLowerCase();
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'AcXZ' : (letters[(Math.random(0-25)*10).toFixed(0)]).toUpperCase();
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Asdr' : (letters[(Math.random(0-25)*10).toFixed(0)]).toUpperCase();
-    str+=letters[(Math.random(0-25)*10).toFixed(0)]===undefined ? 'Add' : (letters[(Math.random(0-25)*10).toFixed(0)]).toLowerCase();
-    setPassword(str)
+    for(let i=0; i<15; i++){
+      let ind = Math.floor(Math.random()*100)%63
+      str+=temp[ind]
+    }
+    console.log(str)  
   }
   const copyPassword = (e)=>{
     if(password!=='')alert(`Success copied  ${password}`)
